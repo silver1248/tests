82b48aa6344dd0c3c642e1083a335c500f9ea5a8
Dorian Abraham <dabraham3@bostonk12.org>
2018-06-06 19:28:00 -0400

    ALMOST DONE

--- src/main/java/testRun/CS106A2Karel.java
+++ src/main/java/testRun/CS106A2Karel.java
@@ -1,40 +1,79 @@
 package testRun;
 
+
 public class CS106A2Karel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	private void gotowall(){
-		if (noBeepersPresent()) {
-			putBeeper
+
+		turnLeft();
+		goToCeiling();
+		goToFloor();
+		goToNextColumn();
+		goToCeiling();
+		goToFloor();
+		goToNextColumn();
+		goToCeiling();
+		goToFloor();
+		goToNextColumn();
+		goToCeiling();
+		goToFloor();
+		turnLeft();
+	}	
+
+	private static void goToCeiling(){
+		while (notBlockedByWall()){
+			if (noBeepersPresent()) {
+				putBeeper();
+			}
+			move();
+		}
+	}
+
+	private static void goToFloor(){
+		turnAround();
+		while (notBlockedByWall()) {
+			move();
+		}
+	}
+
+	private static void goToNextColumn(){
+		turnLeft();
 		move4();
-		
-				
-	}			
-	private void move4(){
+		turnLeft();
+	}
+
+	private static void move4(){
 		move();
 		move();
 		move();
 		move();
 	}
 
-	private void turnRight(){
-			turnLeft();
-			turnLeft();
-			turnLeft();
-		}
-	private void turnAround(){
-			turnLeft();
-			turnLeft();
-		}
-	private void move(){
-		}
-	private void turnLeft(){
-		}
-	private void pickBeeper(){
-		}
-	private void noBeepersPresent(){
-		}
-	private void putBeeper(){
+	private static void turnAround(){
+		turnLeft();
+		turnLeft();
+	}
+
+	private static void move(){
+		System.out.println("move");
+	}
+
+	private static void turnLeft(){
+		System.out.println("turnLeft");
+	}
+
+	private static boolean noBeepersPresent(){
+		System.out.println("noBeeperspresent");
+		return (true);
+	}
+
+	private static void putBeeper(){
+		System.out.println("putBeeper");
+	}
+
+	private static boolean notBlockedByWall(){
+		System.out.println("notBlokedByWall");
+		return(true);
 	}
 }
