a8f4a3da68f710ffb5f5f6995722f75178b588f6
Dorian Abraham <>
2019-03-17 18:17:01 -0400

    I made it do weird counting but with recursion

--- src/main/java/testRun/RecursiveCounting.java
+++ src/main/java/testRun/RecursiveCounting.java
@@ -2,13 +2,21 @@ package testRun;
 
 public class RecursiveCounting {
     public static void main(String[] args) {
-        count(1, 5);
+        recursiveWeirdCounting(1, 10);
     }
 
     private static void count(int start, int limit) {
         if (start <= limit) {
-            System.out.println(start);
+            System.out.println("\t" + start);
             count(start+1, limit);
+        }
+    }
+  
+    private static void recursiveWeirdCounting(int current, int limit ) {
+        if (current <= limit) {
+            System.out.println(current);
+            count(1, current);
+            recursiveWeirdCounting(current+1, limit);
         }
     }
 }
