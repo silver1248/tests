ab62c5ad1b609c24f62c21e6b99431996f051a82
Dorian Abraham <>
2019-03-21 19:21:23 -0400

    I made it do the same thing as wierd counting.

--- src/main/java/testRun/RecursiveCounting.java
+++ src/main/java/testRun/RecursiveCounting.java
@@ -5,18 +5,26 @@ public class RecursiveCounting {
         recursiveWeirdCounting(1, 10);
     }
 
-    private static void count(int start, int limit) {
+    private static void count(int start, int limit) {//j
         if (start <= limit) {
-            System.out.println("\t" + start);
+            tabs(1, limit);
+            System.out.println(start);
             count(start+1, limit);
         }
     }
   
-    private static void recursiveWeirdCounting(int current, int limit ) {
+    private static void recursiveWeirdCounting(int current, int limit ) {//i
         if (current <= limit) {
             System.out.println(current);
             count(1, current);
             recursiveWeirdCounting(current+1, limit);
+        }
+    }
+    
+    private static void tabs(int tabNum, int limit) {//k
+        if (tabNum <= limit) {
+            System.out.print("\t");
+            tabs(tabNum+1, limit);
         }
     }
 }
