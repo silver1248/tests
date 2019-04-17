627830d3cc4b60edaace3974a84d4091a6b0b176
Dorian Abraham <>
2019-04-17 19:28:14 -0400

    list comprehension experiments

--- src/main/java/types/computers/ComputerFinder.java
+++ src/main/java/types/computers/ComputerFinder.java
@@ -19,8 +19,12 @@ public class ComputerFinder  {
         for (Computer computer : cf.match2(64,2048)) {
             System.out.println(computer);
         }
+        System.out.println(cf.getRam());
+        System.out.println(cf.getHD());
+        System.out.println(cf.getHDAndRam());
     }
-    
+
+
     /**
      * Accepts the minimum amount of ram needed, and the minimum amount of hd needed, and a 
      * list of computers.
@@ -39,9 +43,23 @@ public class ComputerFinder  {
         return matching.reverse();
     }
 
+    public List<Long> getRam() {
+        return computers.map(c -> c.getRamSize());
+    }
+
+    public List<Long> getHD() {
+        return computers.map(c -> c.getHardDriveSize());
+    }
+
+    public List<Long> getHDAndRam() {
+        return computers
+                .filter(c -> c.getRamSize() > 32)
+                .map(c -> c.getHardDriveSize() + c.getRamSize());
+    }
+
     public List<Computer> match2(long minRam, long minHD) {
         return computers
-//                .filter(c -> c.getRamSize() >= minRam && c.getHardDriveSize() >= minHD)
+                //                .filter(c -> c.getRamSize() >= minRam && c.getHardDriveSize() >= minHD)
                 .filter(c -> c.getManufacturer().toUpperCase().startsWith("A"))
                 .map(c -> new Mac(c.getName(), c.getRamSize() * 2, c.getHardDriveSize() * 2));
     }
