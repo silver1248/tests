33e9567130c5312be63aee74385d0b82868aff30
Dorian Abraham <>
2019-04-30 19:47:07 -0400

    homework for tommorrow

--- src/main/java/types/computers/ComputerFinder.java
+++ src/main/java/types/computers/ComputerFinder.java
@@ -1,6 +1,9 @@
 package types.computers;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import io.vavr.collection.List;
+import io.vavr.control.Option;
 import lombok.Value;
 
 @Value
@@ -9,19 +12,27 @@ public class ComputerFinder  {
     List<Computer> computers;
 
     public static void main(String[] args) {
+        Mac mac = new Mac("Mac Pro", 64, 2048);
+
         List<Computer> input = List.of(
+                new PC("AlienwareLonger", "Aroura R7", 64, 1024), 
                 new PC("Corsair", "One i160", 32, 2048),
                 new PC("Alienware", "Aroura R7", 64, 1024), 
                 new Mac("Mac Pro", 64, 2048));
-
+        
         ComputerFinder cf = new ComputerFinder(input);
 
         for (Computer computer : cf.match2(64,2048)) {
             System.out.println(computer);
         }
-        System.out.println(cf.getRam());
-        System.out.println(cf.getHD());
-        System.out.println(cf.getHDAndRam());
+//        System.out.println(cf.getRam());
+//        System.out.println(cf.getHD());
+//        System.out.println(cf.getHDAndRam());
+        
+        System.out.println(cf.computers.distinct().map(c -> c.getHardDriveSize()).fold(0l, (l,m) -> l+m));
+        System.out.println(cf.computers.map(c -> c.getHardDriveSize()).distinct().fold(0l, (l,m) -> l+m));
+        System.out.println(cf.computers.contains(mac));
+        System.out.println(cf.biggestAddition());
     }
 
 
@@ -43,23 +54,62 @@ public class ComputerFinder  {
         return matching.reverse();
     }
 
-    public List<Long> getRam() {
-        return computers.map(c -> c.getRamSize());
-    }
-
-    public List<Long> getHD() {
-        return computers.map(c -> c.getHardDriveSize());
-    }
-
-    public List<Long> getHDAndRam() {
+//    public List<Long> getRam() {
+//        return computers.map(c -> c.getRamSize());
+//    }
+//
+//    public List<Long> getHD() {
+//        return computers.map(c -> c.getHardDriveSize());
+//    }
+//
+//    public List<Long> getHDAndRam() {
+//        return computers
+//                .filter(c -> c.getRamSize() > 32)
+//                .map(c -> c.getHardDriveSize() + c.getRamSize());
+//    }
+//    
+    
+    public long biggestAddition() {        
         return computers
-                .filter(c -> c.getRamSize() > 32)
-                .map(c -> c.getHardDriveSize() + c.getRamSize());
+                .map(c -> c.getHardDriveSize() + c.getRamSize())
+                .max()
+                .get();
+    }
+
+    
+    /*
+     * 1. The name and manufacturer together with a space between them(All computers)
+     * 2. Longest printout of #1
+     * 3. Shortest of #1
+     * 4. all longer than Random number (10 - 20)
+     * 5. The name and manufacturer together with a space between them when
+     * manufacturer is more than 6 characters (or something)
+     * 6. ram and manufacturer when the ram size times 2 if bigger than 64
+     */
+    
+    public Computer longestName() {
+        
+         List<Tuple2<Computer, Integer>> foo = computers
+        .map(c -> Tuple.of(c, c.toString().length()));
+//        bar = foo;
+        
+        return computers
+                .map(c -> Tuple.of(c, c.toString().length()))
+                .maxBy(t -> t._2())
+                .get()
+                ._1();
+    }
+
+    public int longestInput() {
+        return computers
+                .map(c -> c.toString().length())
+                .max()
+                .get();
     }
 
     public List<Computer> match2(long minRam, long minHD) {
         return computers
-                //                .filter(c -> c.getRamSize() >= minRam && c.getHardDriveSize() >= minHD)
+                .filter(c -> c.getRamSize() >= minRam && c.getHardDriveSize() >= minHD)
                 .filter(c -> c.getManufacturer().toUpperCase().startsWith("A"))
                 .map(c -> new Mac(c.getName(), c.getRamSize() * 2, c.getHardDriveSize() * 2));
     }
