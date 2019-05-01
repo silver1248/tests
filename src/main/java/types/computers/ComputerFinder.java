76ef777a7c47a69fb0c3015aa6a76a933d103f3e
Dorian Abraham <>
2019-05-01 19:06:19 -0400

    Half of homework done

--- src/main/java/types/computers/ComputerFinder.java
+++ src/main/java/types/computers/ComputerFinder.java
@@ -19,20 +19,23 @@ public class ComputerFinder  {
                 new PC("Corsair", "One i160", 32, 2048),
                 new PC("Alienware", "Aroura R7", 64, 1024), 
                 new Mac("Mac Pro", 64, 2048));
-        
+
         ComputerFinder cf = new ComputerFinder(input);
 
         for (Computer computer : cf.match2(64,2048)) {
             System.out.println(computer);
         }
-//        System.out.println(cf.getRam());
-//        System.out.println(cf.getHD());
-//        System.out.println(cf.getHDAndRam());
-        
+        //        System.out.println(cf.getRam());
+        //        System.out.println(cf.getHD());
+        //        System.out.println(cf.getHDAndRam());
+
         System.out.println(cf.computers.distinct().map(c -> c.getHardDriveSize()).fold(0l, (l,m) -> l+m));
         System.out.println(cf.computers.map(c -> c.getHardDriveSize()).distinct().fold(0l, (l,m) -> l+m));
         System.out.println(cf.computers.contains(mac));
         System.out.println(cf.biggestAddition());
+        System.out.println(cf.getNameAndMan());
+        System.out.println(cf.longestNameAndMan());
+        System.out.println(cf.shortestNameAndMan());
     }
 
 
@@ -54,21 +57,21 @@ public class ComputerFinder  {
         return matching.reverse();
     }
 
-//    public List<Long> getRam() {
-//        return computers.map(c -> c.getRamSize());
-//    }
-//
-//    public List<Long> getHD() {
-//        return computers.map(c -> c.getHardDriveSize());
-//    }
-//
-//    public List<Long> getHDAndRam() {
-//        return computers
-//                .filter(c -> c.getRamSize() > 32)
-//                .map(c -> c.getHardDriveSize() + c.getRamSize());
-//    }
-//    
-    
+    //    public List<Long> getRam() {
+    //        return computers.map(c -> c.getRamSize());
+    //    }
+    //
+    //    public List<Long> getHD() {
+    //        return computers.map(c -> c.getHardDriveSize());
+    //    }
+    //
+    //    public List<Long> getHDAndRam() {
+    //        return computers
+    //                .filter(c -> c.getRamSize() > 32)
+    //                .map(c -> c.getHardDriveSize() + c.getRamSize());
+    //    }
+    //    
+
     public long biggestAddition() {        
         return computers
                 .map(c -> c.getHardDriveSize() + c.getRamSize())
@@ -76,23 +79,38 @@ public class ComputerFinder  {
                 .get();
     }
 
+    public List<String> getNameAndMan() {
+        return computers
+                .map(c -> c.getManufacturer() + " " + c.getName());
+    }
+    
+    public String longestNameAndMan() {
+        return getNameAndMan()
+                .map(s -> Tuple.of(s, s.length()))
+                .maxBy(t -> t._2())
+                .get()
+                ._1();
+    }
+    
+    public String shortestNameAndMan() {
+        return getNameAndMan()
+                .map(s -> Tuple.of(s, s.length()))
+                .minBy(t -> t._2())
+                .get()
+                ._1();
+    }
     
     /*
      * 1. The name and manufacturer together with a space between them(All computers)
      * 2. Longest printout of #1
      * 3. Shortest of #1
-     * 4. all longer than Random number (10 - 20)
+     * 4. all longer than Random number (between 10 and 20)
      * 5. The name and manufacturer together with a space between them when
      * manufacturer is more than 6 characters (or something)
      * 6. ram and manufacturer when the ram size times 2 if bigger than 64
      */
-    
+
     public Computer longestName() {
-        
-         List<Tuple2<Computer, Integer>> foo = computers
-        .map(c -> Tuple.of(c, c.toString().length()));
-//        bar = foo;
-        
         return computers
                 .map(c -> Tuple.of(c, c.toString().length()))
                 .maxBy(t -> t._2())
