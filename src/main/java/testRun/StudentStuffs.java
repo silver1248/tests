381de2dedf4402887f9a5b0bee3a341aa99a28e5
Dorian Abraham <>
2019-06-19 09:35:05 -0400

    Making a program that takes a random number of credits needed to graduate and determines if that student can graduate. Every student also has a GPA and name

--- src/main/java/testRun/StudentStuffs.java
+++ src/main/java/testRun/StudentStuffs.java
@@ -1,11 +1,14 @@
 package testRun;
 
+import io.vavr.collection.List;
+
 public class StudentStuffs {
+    
     /**
      * Student Class
      * each student has
-     * GPA   name
-     * # of credits completed
+     * -GPA   -name
+     * -# of credits completed
      *
      * All students share
      * #of credits needed to graduate
@@ -14,5 +17,21 @@ public class StudentStuffs {
      * Want program where create bunch of students
      * and print out whether they can graduate or not
      */
+    public static void main(String[] args) {
+//        StudentStuffs numOfCreditsCompleted = new StudentStuffs(256);
+    
+        List.of(
+                "Fred",
+                "Bob",
+                "Susan",
+                "Ellen"
+                );
+    
+    }
+    
+    public StudentStuffs(int numOfCreditsCompleted) {
+        
+    }
+    
     
 }
