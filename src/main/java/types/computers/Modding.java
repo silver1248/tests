1910a5fc7646867501a767cb68651f6c41ed1255
Dorian Abraham <>
2019-09-29 19:23:44 -0400

    calling inner method

--- src/main/java/types/computers/Modding.java
+++ src/main/java/types/computers/Modding.java
@@ -3,22 +3,24 @@ package types.computers;
 public class Modding {
     
     public static void main(String[] args) {
-        System.out.println(modEquation(3, 5));
+        System.out.println(modEquation(5, 3));
     }
 
 
-    public static String modEquation(int modBy, int modded) {
+    public static String modEquation(int modded, int modBy) {
         String result = "";
         for (int i = 0; i < modBy; i++) {
             for (int j = 0; j < modded; j++) {
-                int innerModded = modded -j;
-                int innerModBy = modBy - i;
-                String innerString = String.format("%d %% %d = %d\t", innerModded, innerModBy, (innerModded % innerModBy));
-                result += innerString;
+                result += createModLine(modded - j, modBy - i);
             }
             result += "\n";
         }
         return result;
+    }
+
+
+    private static String createModLine(int modded, int modBy) {
+        return String.format("%d %% %d = %d\t", modded, modBy, (modded % modBy));
     }
     
 //    public static int modGrid(int modBy, int modded) {
