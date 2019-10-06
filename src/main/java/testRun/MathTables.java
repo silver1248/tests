288cb7402ce393b0324037e0d8319c0cfb997465
Dorian Abraham <>
2019-10-06 18:08:55 -0400

    I had to pull things to get it back up to date so i made the code better while i was at it

--- src/main/java/testRun/MathTables.java
+++ src/main/java/testRun/MathTables.java
@@ -4,33 +4,26 @@ public class MathTables {
 	public static void main(String[] args) {
 		System.out.println(modGrid(7, 4));
 	}
-	
+
 	public static String modGrid(int multiplicand, int multiplier) {
-		StringBuffer grid = new StringBuffer();
+        StringBuffer grid = new StringBuffer();
 
-			grid.append(thingy(multiplicand));
-			for (int i = 1; i <= multiplier; i++) {
-				grid.append( i + "|");
-				for (int j = 1; j <= multiplicand; j++) {
-				grid.append(createModGrid( i,  j));
-			}
-			grid.append("\n");
-		}
+        for (int i = multiplier; i >= 1; i--) {
+            grid.append("\t ").append(i);
+        }
+        grid.append("\n");
+        for (int i = multiplier; i >= 1; i--) {
+            grid.append(i + "|");
+            for (int j = multiplier; j >= 1; j--) {
+                grid.append(createModGridEntry(i, j));
+            }
+            grid.append("\n");
+        }
 
-		return grid.toString();
-	}
+        return grid.toString();
+    }
 
-	public static String thingy(int multiplicand) {
-		StringBuffer grid = new StringBuffer();
-		for(int i = 1; i <= multiplicand; i++) {
-			grid.append("\t ");
-			grid.append(i);
-		}
-		grid.append("\n");
-		return grid.toString();
-	}
-
-	public static String createModGrid(int multiplicand, int multiplier) {
-		return String.format("\t %d", multiplier * multiplicand);
-	}
+    public static String createModGridEntry(int multiplicand, int multiplier) {
+        return String.format("\t %d", multiplier * multiplicand );
+    }
 }
