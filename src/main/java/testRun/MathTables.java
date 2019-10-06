f2ef5bef10bce3011f18b4cee8eb36bcda7df286
Dorian Abraham <>
2019-10-06 18:19:30 -0400

    made it so that you can enter a third number and if that number appears on the board there will be one * one each side of the number

--- src/main/java/testRun/MathTables.java
+++ src/main/java/testRun/MathTables.java
@@ -2,28 +2,32 @@ package testRun;
 
 public class MathTables {
 	public static void main(String[] args) {
-		System.out.println(modGrid(7, 4));
+		System.out.println(modGrid(7, 4, 13));
 	}
 
-	public static String modGrid(int multiplicand, int multiplier) {
-        StringBuffer grid = new StringBuffer();
+	public static String modGrid(int multiplicand, int multiplier, int product) {
+		StringBuffer grid = new StringBuffer();
 
-        for (int i = multiplier; i >= 1; i--) {
-            grid.append("\t ").append(i);
-        }
-        grid.append("\n");
-        for (int i = multiplier; i >= 1; i--) {
-            grid.append(i + "|");
-            for (int j = multiplier; j >= 1; j--) {
-                grid.append(createModGridEntry(i, j));
-            }
-            grid.append("\n");
-        }
+		for (int i = multiplier; i >= 1; i--) {
+			grid.append("\t ").append(i);
+		}
+		grid.append("\n");
+		for (int i = multiplier; i >= 1; i--) {
+			grid.append(i + "|");
+			for (int j = multiplier; j >= 1; j--) {
+				if(i * j == product) {
+					grid.append("\t*").append(createModGridEntry(i, j)).append("*");
+				} else {
+					grid.append("\t").append(createModGridEntry(i, j));
+				}
+			}
+			grid.append("\n");
+		}
 
-        return grid.toString();
-    }
+		return grid.toString();
+	}
 
-    public static String createModGridEntry(int multiplicand, int multiplier) {
-        return String.format("\t %d", multiplier * multiplicand );
-    }
+	public static String createModGridEntry(int multiplicand, int multiplier) {
+		return String.format("%d", multiplier * multiplicand );
+	}
 }
