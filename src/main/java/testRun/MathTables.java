4a0d96b6972b38d77b8855c71d25dd328a6fd88f
Dorian Abraham <>
2019-10-13 17:35:08 -0400

    made a loop so that there will always be a line below the top row of numbers 1 - less than how long it is because it wouldn't let a loop make it go all the way and I'm to lazy to write an extra append outside of a loop to fix it

--- src/main/java/testRun/MathTables.java
+++ src/main/java/testRun/MathTables.java
@@ -2,7 +2,7 @@ package testRun;
 
 public class MathTables {
 	public static void main(String[] args) {
-		System.out.println(modGrid(7, 4, 12));
+		System.out.println(modGrid(7, 12, 12));
 	}
 
 	public static String modGrid(int multiplicand, int multiplier, int product) {
@@ -12,13 +12,17 @@ public class MathTables {
 			grid.append("\t").append(i);
 		}
 		grid.append("\n");
+		for(int l = 1; l <= multiplier; l++) {
+		    grid.append("---------");
+		}
+		grid.append("\n");
 		for (int i = 1; i <= multiplier; i++) {
 			grid.append(i + "|");
 			for (int j = 1; j <= multiplier; j++) {
 				if(i * j == product) {
-					grid.append("\t*").append(createModGridEntry(i, j)).append("*");
+					grid.append("\t*").append(createGridEntry(i, j)).append("*");
 				} else {
-					grid.append("\t").append(createModGridEntry(i, j));
+					grid.append("\t").append(createGridEntry(i, j));
 				}
 			}
 			grid.append("\n");
@@ -27,7 +31,7 @@ public class MathTables {
 		return grid.toString();
 	}
 
-	public static String createModGridEntry(int multiplicand, int multiplier) {
+	public static String createGridEntry(int multiplicand, int multiplier) {
 		return String.format("%d", multiplier * multiplicand );
 	}
 }
