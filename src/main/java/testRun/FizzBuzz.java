8152f024113a3e9ad7b9fcc3c295bf12d375d6a7
Dorian Abraham <>
2019-10-13 18:53:20 -0400

    made a new much better fizzBuzz method because the original one was bad despite working

--- src/main/java/testRun/FizzBuzz.java
+++ src/main/java/testRun/FizzBuzz.java
@@ -10,17 +10,36 @@ public class FizzBuzz {
         StringBuffer numLine = new StringBuffer();
 
         for (int i = 1; i <= stopNum; i++) {
+            boolean mod = false;
+            if (i % fizz == 0) {
+                numLine.append("fizz");
+                mod = true;
+            }
+            if (i % buzz == 0) {
+                numLine.append("buzz");
+                mod = true;
+            }
+            if (!mod) {
+                numLine.append(i);
+            }
+            numLine.append("\n");
+            
+            
+            
+            
+            
+            
+            
             if ((i % fizz) + (i % buzz) == 0) {
                 numLine.append("fizzBuzz").append("\n");
             } else {
                 if (i % fizz == 0) {
                     numLine.append("fizz").append("\n");
+                }
+                if (i % buzz == 0) {
+                    numLine.append("buzz").append("\n");
                 } else {
-                    if (i % buzz == 0) {
-                        numLine.append("buzz").append("\n");
-                    } else {
                         numLine.append(i).append("\n");
-                    }
                 }
             }
         }
