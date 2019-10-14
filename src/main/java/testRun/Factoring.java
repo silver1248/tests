85abe46b8f4285cc33c790e403f42bf5b94af1d2
Dorian Abraham <>
2019-10-14 19:20:36 -0400

    made the code shorter, better, and more readable

--- src/main/java/testRun/Factoring.java
+++ src/main/java/testRun/Factoring.java
@@ -3,16 +3,21 @@ package testRun;
 public class Factoring {
 
     public static void main(String[] args) {
-        factor(10);
+        factorLoop(10);
     }
-    public static int factor(int stop) {
-        for(int i = 1; i <= stop; i++) {
-            for(int factorNumber = 1;factorNumber <= Math.sqrt(i) ; factorNumber++){
-                if(i % factorNumber == 0){
-                    System.out.println(factorNumber + "*" + i / factorNumber + "=" + i);
-                }
-            }
+    public static int factorLoop(int stop) {
+        for (int i = 1; i <= stop; i++) {
+            factor(i);
         }
         return stop;
+    }
+    
+    public static int factor(int number) {
+        for (int factorNumber = 1; factorNumber <= Math.sqrt(number); factorNumber++) {
+            if(number % factorNumber == 0){
+                System.out.printf("%s * %s = %s\n", factorNumber, number / factorNumber, number);
+            }
+        }
+        return number;
     }
 }
