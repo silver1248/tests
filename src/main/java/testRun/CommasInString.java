8f2aea2140aeeb46b851713e9a5c829e1969df0a
Dorian Abraham <>
2019-10-17 18:59:04 -0400

    removed extraneous traces

--- src/main/java/testRun/CommasInString.java
+++ src/main/java/testRun/CommasInString.java
@@ -6,26 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 public class CommasInString {
 
 	public static void main(String[] args) {
-		log.trace("dmma main 000");
-		addCommasToString("dfdf");
-		log.trace("dmma main 010");
+		addCommasToString("1385792");
 	}
 	
 	public static String addCommasToString(String figures) {
-		log.trace("dmma addCommasToString 000");
-		int i = figures.length();
-		if (i < 0) {
-			log.error("string has negative length, fix that!");
+		int length = figures.length();
+
+		for (int i = 0; i <= length; i++) {
+			log.trace("dmma addCommasToString 020 i = {}, j = {}", length, i);
+			
 		}
-		log.trace("dmma addCommasToString 010 i = {}",i);
-		for(int j = 0; j <= i; j++) {
-			log.trace("dmma addCommasToString 020 i = {}, j = {}", i, j);
-			int i2 = j;
-			log.trace("dmma addCommasToString 030 i = {}, j = {}", i, j);
-			i2 = i2 +1;
-			log.trace("dmma addCommasToString 040 i = {}, j = {}", i, j);
-		}
-		log.trace("dmma addCommasToString 999");
 		return "";
 	}
 }
