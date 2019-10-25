34f1fb418e3d4c532a4e3bca4f25fd6109ee45d8
Dorian Abraham <>
2019-10-25 15:30:16 -0400

    started turning the string from the last commit into a commas in a string program

--- src/main/java/testRun/CommasInString.java
+++ src/main/java/testRun/CommasInString.java
@@ -7,7 +7,8 @@ public class CommasInString {
 
 	public static void main(String[] args) {
 		addCommasToString("1385792");
-        System.out.println(experiment("hamburger","burg"," Arugala is a vejitabel "));
+        System.out.println(experiment("hamburger","burg"," Arugala is a vejitabul "));
+        System.out.println(attemptAtCommas("1000000", "000000", ","));
 	}
 	
 	public static String addCommasToString(String figures) {
@@ -32,6 +33,29 @@ public class CommasInString {
              repl + 
              str.substring(index + orig.length());
         }
+        return str;
+    }
+	
+	public static String attemptAtCommas(String str, String orig, String repl) {
+        int index = str.indexOf(orig);
+        int j = str.length();
+        
+        log.debug(str.substring(0, index));
+        log.debug(repl);
+        log.debug(str.substring(index + orig.length()));
+
+        for (int i = 0; i < str.length(); i++) {
+        	if (i % 3 == 0) {
+        		str = str.substring(0, i) +
+        		repl + str.substring(i + 1, j);
+        	}
+        }
+        
+        if (index != -1) {
+            str = str.substring(0, index) + 
+            repl + 
+            str.substring(index + orig.length());
+       }
         return str;
     }
 }
