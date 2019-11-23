28a5465b25b4ae3266c4b34f2d3849286ae310ff
Dorian Abraham <>
2019-11-23 16:31:40 -0500

    i think taht i've made some pretty good progress on the commas in a string with decimals program

--- src/main/java/testRun/CommasInStringDecimal.java
+++ src/main/java/testRun/CommasInStringDecimal.java
@@ -6,34 +6,36 @@ import lombok.extern.slf4j.Slf4j;
 public class CommasInStringDecimal {
 
     public static void main(String[] args) {
-        System.out.println(commasInString("3566575.1967"));
-        System.out.println(commasInString("3"));
-        System.out.println(commasInString("345"));
-        System.out.println(commasInString("34.56"));
-        System.out.println(commasInString("1234.56"));
-        System.out.println(commasInString("12345.67"));
-        System.out.println(commasInString("456749.8432"));
-        System.out.println(commasInString("3826498.73641"));
-        System.out.println(commasInString("84326198.74612"));
-        System.out.println(commasInString("509327698.1563287493215913649"));
-        System.out.println(commasInString("9372984799.26748621576981"));
+        System.out.println(commasInString("1"));
+        System.out.println(commasInString("12"));
+        System.out.println(commasInString("123"));
+        System.out.println(commasInString("12.34"));
+        System.out.println(commasInString("1234.5"));
+        System.out.println(commasInString("12345.6"));
+        System.out.println(commasInString("123.4567"));
+        System.out.println(commasInString("1234.5678"));
+        System.out.println(commasInString("12345.6789"));
+        System.out.println(commasInString("123456.7890"));
 
     }
 
     public static String commasInString(String str) {
         int length = str.length();
+        int lengthToComma = str.indexOf('.');
         int numberOfCommas = (length-1) / 3;
-
         log.trace("length = {}, commas = {}", length, numberOfCommas);
 
-        for (int i = 1; i <= numberOfCommas; i ++) {
-            str = str.substring(0, length - (i*3)) + "," + str.substring(length - (i*3));
-            if (str.indexOf(".") >= i) {
-                System.out.println(str.substring(i));
-                break;
+        if(str.indexOf('.') != -1) {
+            for (int i = lengthToComma; i >= lengthToComma; i --) {
+                str = str.substring(0, lengthToComma - (i/3)) + "," + str.substring(length - (i*3));
             }
-        }
 
+        } else {
+
+            }
+            for (int i = 1; i <= numberOfCommas; i ++) {
+                str = str.substring(0, length - (i*3)) + "," + str.substring(length - (i*3));
+            }
         return str;
     }
 }
