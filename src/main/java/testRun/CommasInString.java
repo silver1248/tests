7181732f0809fa69950cfd12cc5489e7e8f51a6f
Dorian Abraham <>
2019-11-29 21:36:05 -0500

    extracted tests from main to unit tests

--- src/main/java/testRun/CommasInString.java
+++ src/main/java/testRun/CommasInString.java
@@ -5,21 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CommasInString {
 
-    public static void main(String[] args) {
-        System.out.println(commasInString("35665751967"));
-        System.out.println(commasInString("3"));
-        System.out.println(commasInString("345"));
-        System.out.println(commasInString("3456"));
-        System.out.println(commasInString("123456"));
-        System.out.println(commasInString("1234567"));
-        System.out.println(commasInString("4567498432"));
-        System.out.println(commasInString("382649873641"));
-        System.out.println(commasInString("8432619874612"));
-        System.out.println(commasInString("5093276981563287493215913649"));
-        System.out.println(commasInString("937298479926748621576981"));
-
-    }
-
     public static String commasInString(String str) {
         if (str == null) {
             return "";
