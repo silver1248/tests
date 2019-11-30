64b144839cfc67523a9716bf10679a8400274da2
Dorian Abraham <>
2019-11-29 21:37:18 -0500

    fixed bug

--- src/main/java/testRun/CommasInStringDecimal.java
+++ src/main/java/testRun/CommasInStringDecimal.java
@@ -5,19 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CommasInStringDecimal {
 
-    public static String commasInString(String str) {
-        int length = str.length();
-        int lengthToDecimal = str.indexOf('.');
-        int numberOfCommas = (length-1) / 3;
-
-        for (int i = 1; i <= numberOfCommas; i ++) {
-            log.trace("debug 3 i = {}, length = {}, lengthToDecimal", i, length, lengthToDecimal);
-            str = str.substring(0, length - (i*3)) + "," + str.substring(length - (i*3));
-            log.trace("debug 4 i = {}, length = {}, lengthToDecimal", i, length, lengthToDecimal);
-        }
-        return str;
-    }
-
 
     public static String commasInStringWithDecimal(String str) {
         if (str == null) {
@@ -25,17 +12,17 @@ public class CommasInStringDecimal {
         }
         int length = str.length();
         int lengthToDecimal = str.indexOf('.');
+        if (lengthToDecimal == -1) {
+            lengthToDecimal = length;
+        }
         int numberOfCommas = (lengthToDecimal-1) / 3;
         log.trace("length = {}, commas = {}", length, numberOfCommas);
 
             for (int i = 1; i <= numberOfCommas; i ++) {
                 int substringReference = lengthToDecimal - (i*3);
                 log.trace("debug 1 subStringReference = {} i = {}, length = {}", substringReference, i, lengthToDecimal);
-                if (substringReference != -1) {
-
-                    str = str.substring(0, substringReference) + "," + str.substring(lengthToDecimal - (i*3));
-                    log.trace("debug 2 subStringReference = {}, i = {}, length = {}", substringReference, i, lengthToDecimal);
-                }
+                str = str.substring(0, substringReference) + "," + str.substring(substringReference);
+                log.trace("debug 2 subStringReference = {}, i = {}, length = {}", substringReference, i, lengthToDecimal);
             }
 
         return str;
--- src/test/java/testRun/CommasInStringDecimalTest.java
+++ src/test/java/testRun/CommasInStringDecimalTest.java
@@ -1,15 +1,21 @@
 package testRun;
 
 import static org.testng.Assert.assertEquals;
+import static testRun.CommasInStringDecimal.commasInStringWithDecimal;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CommasInStringDecimalTest {
 
+    @Test
+    public void instantiationTest() {
+        new CommasInStringDecimal();
+    }
+
     @Test(dataProvider="commasInStringTestDP")
     public void commasInStringWithDecimalTest(String in, String expected) {
-        assertEquals(CommasInStringDecimal.commasInStringWithDecimal(in), expected);
+        assertEquals(commasInStringWithDecimal(in), expected);
     }
 
     @DataProvider
@@ -24,10 +30,10 @@ public class CommasInStringDecimalTest {
             {"12345", "12,345"},
             {"1234567", "1,234,567"},
 
-            {"123.45678", },
+            {"123.45678", "123.45678"},
             {"1234.5678", "1,234.5678"},
             {"12345.67890", "12,345.67890"},
-            {"123456789.46536454652", "1,234,567,890.46536454652"},
+            {"123456789.46536454652", "123,456,789.46536454652"},
         };
     }
 }
