e65118992c2b446c943ff369db077f5926d56bf8
Dorian Abraham <>
2019-12-08 19:35:55 -0500

    made a thing that prints an array of numbers from x to y

--- src/main/java/testRun/ArrayPractice.java
+++ src/main/java/testRun/ArrayPractice.java
@@ -1,15 +1,17 @@
 package testRun;
 
+import java.util.Arrays;
+
 public class ArrayPractice {
 
     public static void main(String[] args) {
-        System.out.println(arrayPractice(37, 20));
+        System.out.println(Arrays.toString(arrayPractice(37, 20)));
     }
 
     public static int[] arrayPractice(int start, int length) {
 
         int intArray[] = new int[length];
-        for (int i = start; i > start + length; i++) {
+        for (int i = start; i < start + length; i++) {
             intArray[i - start] = i;
         }
 //        int a[]=new int[5];
