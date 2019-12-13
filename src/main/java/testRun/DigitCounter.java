6dc18601efb2340800cb907b1efcd70a54a36300
Dorian Abraham <>
2019-12-12 19:38:26 -0500

    cleaned code up a bit

--- src/main/java/testRun/DigitCounter.java
+++ src/main/java/testRun/DigitCounter.java
@@ -3,19 +3,17 @@ package testRun;
 public class DigitCounter {
 
     public static int[] count(String s) {
+
         int countArray[] = new int[10];
-        if (s == null) {
-            return countArray;
+
+        if (null != s) {
+            for (int i = 0; i < s.length(); i++) {
+                String charAt = s.substring(i, i+1);
+                int digit = Integer.parseInt(charAt);
+                countArray[digit]++;
+            }
         }
-        
-        for (int i = 0; i < s.length(); i++) {
-            String charAt = s.substring(i, i+1);
-            int digit = Integer.parseInt(charAt);
-            
-            countArray[digit]++;
-            
-        }
-        
+
         return countArray;
     }
 
