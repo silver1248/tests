69040cd2ebd1a51882759d471cb42042473d16ee
Dorian Abraham <>
2019-12-12 19:30:48 -0500

    MADE A THING THAT TELLS YOU HOW MANY TIMES EACH DIGIT IN A NUERIC STRING IS USED IN THE STRING

--- src/main/java/testRun/DigitCounter.java
+++ src/main/java/testRun/DigitCounter.java
@@ -3,7 +3,20 @@ package testRun;
 public class DigitCounter {
 
     public static int[] count(String s) {
-        return null;
+        int countArray[] = new int[10];
+        if (s == null) {
+            return countArray;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            String charAt = s.substring(i, i+1);
+            int digit = Integer.parseInt(charAt);
+            
+            countArray[digit]++;
+            
+        }
+        
+        return countArray;
     }
 
 }
