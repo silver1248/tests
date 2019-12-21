a178ed9aad4e4ff3c329615ecffcd55d90c99b61
Dorian Abraham <>
2019-12-20 20:11:10 -0500

    FINALLY MADE THE METHOD THAT MAKES A HISTOGRAM THAT SHOWS THE AMOUNT OF TIMES A DIGIT WAS USED IN A STRING WORK!!!!!!

--- src/main/java/testRun/ArrayPracticeButPretty.java
+++ src/main/java/testRun/ArrayPracticeButPretty.java
@@ -7,17 +7,27 @@ public class ArrayPracticeButPretty {
 
 
 
-	public static String prettyArray(String in) {
-		int[] countArray = DigitCounter.count(in);
-		
-		for (int i = 0; i < 10; i++) {
-			
-		}
-		System.out.println("%s |");
-		makeHistogram();
-	}
+    public static String prettyArray(String in) {
+        int[] countArray = DigitCounter.count(in);
+        StringBuffer sb = new StringBuffer();
 
-	public static String makeHistogram(){
-		System.out.println("0| \n1| \n2| \n3| \n4| \n5| \n6| \n7| \n8| \n9| ");
-	}
+        for (int i = 0; i < 10; i++) {
+            sb.append(histogramLine(countArray, i));
+        }
+
+        return sb.toString();
+    }
+
+    public static String histogramLine(int[] countArray, int i) {
+        return i + "| " + stars(countArray[i]) + "\n";
+    }
+
+    private static String stars(int i) {
+        StringBuffer sb = new StringBuffer();
+        for (int j = 1; j <= i; j++) {
+            sb.append("*");
+        }
+        return sb.toString();
+    }
+
 }
--- src/test/java/testRun/ArrayPracticeButPrettyTest.java
+++ src/test/java/testRun/ArrayPracticeButPrettyTest.java
@@ -8,20 +8,21 @@ import org.testng.annotations.Test;
 
 public class ArrayPracticeButPrettyTest {
 
-	@Test (dataProvider = "countTestButPrettyDP")
-	public void countTest(String in, String expected) {
-		assertEquals(ArrayPracticeButPretty.prettyArray(in), expected);
-	}
+    @Test (dataProvider = "countTestButPrettyDP")
+    public void countTest(String in, String expected) {
+        assertEquals(ArrayPracticeButPretty.prettyArray(in), expected);
+    }
 
-	@DataProvider
-	Object[][] commasInStringTestDP() {
-		String emptyHistogram = "0| \n1| \n2| \n3| \n4| \n5| \n6| \n7| \n8| \n9| ";
-		return new Object[][] {
-			{null, emptyHistogram},
-			{"", emptyHistogram},
+    @DataProvider
+    Object[][] countTestButPrettyDP() {
+        String emptyHistogram = "0| \n1| \n2| \n3| \n4| \n5| \n6| \n7| \n8| \n9| \n";
+        return new Object[][] {
+            {null, emptyHistogram},
+            {"", emptyHistogram},
 
-			{"0", "0| *\n1| \n2| \n3| \n4| \n5| \n6| \n7| \n8| \n9| "},
-			{"101", "0| *\n1| **\n2| \n3| \n4| \n5| \n6| \n7| \n8| \n9| "},
-		};
-	}
+            {"0", "0| *\n1| \n2| \n3| \n4| \n5| \n6| \n7| \n8| \n9| \n"},
+            {"101", "0| *\n1| **\n2| \n3| \n4| \n5| \n6| \n7| \n8| \n9| \n"},
+            {"31415926", "0| \n1| **\n2| *\n3| *\n4| *\n5| *\n6| *\n7| \n8| \n9| *\n"},
+        };
+    }
 }
