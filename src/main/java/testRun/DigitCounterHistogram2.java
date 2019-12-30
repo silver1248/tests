c403e21e38163410bdbc178ab9a53386cb99886f
Dorian Abraham <>
2019-12-30 12:29:45 -0500

    making the histogram but sideways

--- src/main/java/testRun/DigitCounterHistogram2.java
+++ src/main/java/testRun/DigitCounterHistogram2.java
@@ -1,28 +1,39 @@
 package testRun;
 
 public class DigitCounterHistogram2 {
-    
-	public static String prettyArray(String in) {
+
+    public static String prettyArray(String in) {
         int[] countArray = DigitCounter.count(in);
         StringBuffer sb = new StringBuffer();
 
-        for (int i = 0; i < 10; i++) {
-            sb.append(histogramLine(countArray, i));
+        if (in != null || !"".equals(in)) {
+
+            for (int i = 0; i < 10; i++) {
+                sb.append(histogramLine(countArray, i));
+            }
         }
 
         return sb.toString();
     }
 
     public static String histogramLine(int[] countArray, int i) {
-        return String.format(stars(countArray[i]),"%d| %s",i);
+        return String.format(" %s\n- - - \n%d",stars(countArray[i]),i);
     }
 
     private static String stars(int i) {
         StringBuffer sb = new StringBuffer();
         for (int j = 1; j <= i; j++) {
-            sb.append("\n*");
+            sb.append("* \n");
         }
         return sb.toString();
+    }
+
+    public static int max(int[] in) {
+        int max = 0;
+        for (int i = 0; i < in.length; i++) {
+            
+        }
+        return max;
     }
 
 }
--- src/test/java/testRun/DigitCounterHistogram2Test.java
+++ src/test/java/testRun/DigitCounterHistogram2Test.java
@@ -1,11 +1,28 @@
 package testRun;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DigitCounterHistogram2Test {
 
-  @Test
-  public void prettyArrayTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @Test (dataProvider = "countTestButPrettyDP")
+    public void countTest(String in, String expected) {
+        assertEquals(DigitCounterHistogram2.prettyArray(in), expected);
+    }
+
+    @DataProvider
+    Object[][] countTestButPrettyDP() {
+        String emptyHistogram = "  - - - - - - - - - - - - - - - - - - \n    0 1 2 3 4 5 6 7 8 9";
+        return new Object[][] {
+            {null, emptyHistogram},
+            {"", emptyHistogram},
+
+            {"0",        "1 | *                  \n  - - - - - - - - - - - - - - - - - - \n    0 1 2 3 4 5 6 7 8 9"},
+            {"101",      "2 |   *                \n1 |  * *               \n  - - - - - - - - - - - - - - - - - - \n    0 1 2 3 4 5 6 7 8 9"},
+            {"3141",     "2 |   *                \n1 |   *   * *          \n    0 1 2 3 4 5 6 7 8 9"},
+            {"31415926", "2 |   *                \n1 |   * * * * * *     *\n  - - - - - - - - - - - - - - - - - - \n    0 1 2 3 4 5 6 7 8 9"},
+        };
+    }
 }
