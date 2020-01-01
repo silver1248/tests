1cddb6703b3a763b7a7781a5be43ade0ea3b5e58
Dorian Abraham <>
2019-12-31 19:27:22 -0500

    MADE THE HORIZONTAL HISTOGRAM WORK!!!!!!!!!! YES FINALLY!!!!!!!!!!

--- src/main/java/testRun/DigitCounterHistogram2.java
+++ src/main/java/testRun/DigitCounterHistogram2.java
@@ -8,22 +8,28 @@ public class DigitCounterHistogram2 {
 
         if (in != null || !"".equals(in)) {
 
-            for (int i = 0; i < 10; i++) {
-                sb.append(histogramLine(countArray, i));
+            for (int i = max(countArray); i > 0; i--) {
+                sb.append(sideLine(countArray, i));
             }
+            sb.append("    -------------------\n")
+              .append("    0 1 2 3 4 5 6 7 8 9");
         }
 
         return sb.toString();
     }
 
-    public static String histogramLine(int[] countArray, int i) {
-        return String.format(" %s\n- - - \n%d",stars(countArray[i]),i);
+    public static String sideLine(int[] countArray, int i) {
+        return String.format("%d |%s\n",i,stars(i, countArray));
     }
 
-    private static String stars(int i) {
+    public static String stars(int i, int[] countArray) {
         StringBuffer sb = new StringBuffer();
-        for (int j = 1; j <= i; j++) {
-            sb.append("* \n");
+        for (int j = 0; j < 10; j++) {
+            if (countArray[j] >= i) {
+                sb.append(" *");
+            } else {
+                sb.append("  ");
+            }
         }
         return sb.toString();
     }
