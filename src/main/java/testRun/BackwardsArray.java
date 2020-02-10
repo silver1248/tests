ecc5494cdc9c95408cbeaf44908151dd7ff65df3
Dorian Abraham <>
2020-02-09 19:43:00 -0500

    Starting making a method that reverses the content of a multi-dimensional array

---
+++ src/main/java/testRun/BackwardsArray.java
@@ -0,0 +1,17 @@
+package testRun;
+
+public class BackwardsArray {
+
+    
+    public static int[][] turnAround(int[][] in) {
+        int d1 = in.length;
+        for (int i = 0; i < d1; i++) {
+            
+//            int d2 = in[i].length;
+//            for (int j = 0; j < d2; j++) {
+//                in
+//            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/testRun/BackwardsArrayTest.java
@@ -0,0 +1,26 @@
+package testRun;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class BackwardsArrayTest {
+
+    @Test(dataProvider="turnAroundTestDP")
+    public void turnAroundTest(int[][] in, int[][] expected) {
+        assertEquals(BackwardsArray.turnAround(in), expected);
+    }
+
+    @DataProvider
+    Object[][] turnAroundTestDP() {
+        return new Object[][] {
+            {new int[0][0],new int[0][0]},
+
+            {new int[][]{{1}}, new int[][]{{1}}},
+            {new int[][]{{5,4,3,2,1}}, new int[][]{{1,2,3,4,5}}},
+
+            {new int[][]{{12,13}, {14,15}}, new int[][]{{13,12}, {15,14}}},
+        };
+    }
+}
