c49d3e983ec2c036feecba130e7d52ac74efcd58
Dorian Abraham <>
2020-02-11 18:24:46 -0500

    I t h i n k that i've made some progress in the array reversing code

--- src/main/java/testRun/BackwardsArray.java
+++ src/main/java/testRun/BackwardsArray.java
@@ -2,16 +2,45 @@ package testRun;
 
 public class BackwardsArray {
 
-    
-    public static int[][] turnAround(int[][] in) {
-        int d1 = in.length;
-        for (int i = 0; i < d1; i++) {
-            
-//            int d2 = in[i].length;
-//            for (int j = 0; j < d2; j++) {
-//                in
-//            }
-        }
-        return null;
-    }
+
+	public static int[][] turnAround(int[][] in) {
+		System.out.print("in ");
+		printArray(in);
+		int d1 = in.length;
+		for (int i = 0; i < d1; i++) {
+			changeIndex(in[i]);
+		}
+		System.out.print("out ");
+		printArray(in);
+		return in;
+	}
+
+	public static void changeIndex(int[] in) {
+		printArray(in);
+		int l = in.length;
+		System.out.println("l " + l);
+		for(int i = 0; i > l / 2; i++) {
+			int temp = in[l - i];
+			in[l - i] = in[i];
+			in[i] = temp;
+		}
+		printArray(in);
+	}
+
+	public static void printArray(int[][] actual) {
+		for (int i = 0; i < actual.length; i++) {
+			for (int j = 0; j < actual[i].length; j++) {    			
+				System.out.print(actual[i][j]+ " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	public static void printArray(int[] actual) {
+		for (int i = 0; i < actual.length; i++) {
+			System.out.print(actual[i]+ " ");
+		}
+		System.out.println();
+	}
 }
--- src/test/java/testRun/BackwardsArrayTest.java
+++ src/test/java/testRun/BackwardsArrayTest.java
@@ -9,18 +9,20 @@ public class BackwardsArrayTest {
 
     @Test(dataProvider="turnAroundTestDP")
     public void turnAroundTest(int[][] in, int[][] expected) {
-        assertEquals(BackwardsArray.turnAround(in), expected);
+    	int[][] actual = BackwardsArray.turnAround(in);
+    	BackwardsArray.printArray(actual);
+        assertEquals(actual, expected);
     }
 
     @DataProvider
     Object[][] turnAroundTestDP() {
         return new Object[][] {
-            {new int[0][0],new int[0][0]},
-
-            {new int[][]{{1}}, new int[][]{{1}}},
+//            {new int[0][0],new int[0][0]},
+//
+//            {new int[][]{{1}}, new int[][]{{1}}},
             {new int[][]{{5,4,3,2,1}}, new int[][]{{1,2,3,4,5}}},
 
-            {new int[][]{{12,13}, {14,15}}, new int[][]{{13,12}, {15,14}}},
+//            {new int[][]{{12,13}, {14,15}}, new int[][]{{13,12}, {15,14}}},
         };
     }
 }
