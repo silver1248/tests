39184e9f39b920e6e30efbb9ca6609ab55dcaf25
Dorian Abraham <>
2020-02-25 19:31:10 -0500

    THIS TIME I MADE PERFECT AMAZING POWER OF 2 GENERATING ITERATOR CODE!!!!!!!!!

--- src/main/java/testRun/PowersOf2Iterator.java
+++ src/main/java/testRun/PowersOf2Iterator.java
@@ -1,5 +1,19 @@
 package testRun;
 
-public class PowersOf2Iterator {
+import java.util.Iterator;
 
+public class PowersOf2Iterator implements Iterator<Integer> {
+
+    int evenInt = 1;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+
+        return evenInt *= 2;
+    }
 }
--- src/test/java/testRun/PowersOf2IteratorTest.java
+++ src/test/java/testRun/PowersOf2IteratorTest.java
@@ -1,16 +1,33 @@
 package testRun;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class PowersOf2IteratorTest {
 
-  @Test
-  public void hasNextTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @Test
+    public void hasNextTest() {
+        PowersOf2Iterator po2i = new PowersOf2Iterator();
+        assertTrue(po2i.hasNext());
+    }
 
-  @Test
-  public void nextTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @Test(dataProvider="nextTestDP")
+    public void nextTest(PowersOf2Iterator po2i, Integer expected) {
+        assertEquals(po2i.next(), expected);
+    }
+
+    @DataProvider
+    Object[][] nextTestDP() {
+        PowersOf2Iterator po2i = new PowersOf2Iterator();
+        return new Object[][] {
+            {po2i, 2},
+            {po2i, 4},
+            {po2i, 8},
+            {po2i, 16},
+            {po2i, 32},
+        };
+    }
 }
