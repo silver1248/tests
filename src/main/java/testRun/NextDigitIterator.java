cf7b4943ecf8d3ecfc94558756b98868e319cec4
Dorian Abraham <>
2020-03-08 18:20:57 -0400

    i think i'm almost done with the NextDigitIterator class and tests

---
+++ src/main/java/testRun/NextDigitIterator.java
@@ -0,0 +1,46 @@
+package testRun;
+
+import java.util.Iterator;
+
+import lombok.Value;
+import lombok.experimental.NonFinal;
+
+@Value
+public class NextDigitIterator implements Iterator {
+
+    @NonFinal int i = 0;
+    int[] digits;
+    int length;
+
+    public int[] stringToArray(String string) {
+        if (string.indexOf('.') != -1) {
+            string = string.substring(0, string.indexOf('.')) + string.substring(string.indexOf('.') + 1);
+        }
+        int[] digits = new int[]{};
+        for (int j = 0; j < string.length(); i++) {
+            if (j != string.length() - 1) {
+                digits[j] = Integer.parseInt(string.substring(j, j + 1));
+            } else {
+                digits[j] = Integer.parseInt(string.substring(j));
+            }
+
+        }
+        return digits;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return i < length;
+    }
+
+    @Override
+    public Object next() {
+        return digits[i++];
+    }
+
+    public NextDigitIterator(String string) {
+        this.digits = stringToArray(string);
+        this.length = digits.length;
+    }
+
+}
---
+++ src/test/java/testRun/NextDigitIteratorTest.java
@@ -0,0 +1,41 @@
+package testRun;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class NextDigitIteratorTest {
+
+    /*The dataProvider will pass a String and an int[]
+The test will use the String to create a NextDigitIterator
+It will then loop through the digits of the array an iterator to check if they’re the same
+It will also than check that the iterator says it doesn’t have anything left
+ */
+    
+    @Test
+    public void hasNextTest() {
+
+    }
+
+    @Test(dataProvider="nextTestDP")
+    public void nextTest(NextDigitIterator ndi, int[] expected) {
+        int length = expected.length;
+        assertEquals(ndi.next(), expected);
+        for(int i = 0; i < length; i++) {
+            assertEquals(Integer.parseInt(ndi.next().toString()), expected[i]);
+        }
+    }
+
+    @DataProvider
+    Object[][] nextTestDP() {
+        return new Object[][] {
+            {new NextDigitIterator(""), new int[] {}},
+            {new NextDigitIterator("26832517872"), new int[] {2,6,8,3,2,5,1,7,8,7,2}},
+            {new NextDigitIterator("3.14159"), new int[] {3,1,4,1,5,9}},
+            {new NextDigitIterator("3820946"), new int[] {3,8,2,0,9,4,6}},
+            {new NextDigitIterator("453027"), new int[] {4,5,3,0,2,7}},
+            {new NextDigitIterator("123456"), new int[] {1,2,3,4,5,6}},
+        };
+    }
+}
