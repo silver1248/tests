296c78fdfbf23037d10acae723e3c84a7cc659b3
Dorian Abraham <>
2020-03-21 19:15:20 -0400

    commiting so that i don't lose the code

--- src/main/java/testRun/NextDigitIterator.java
+++ src/main/java/testRun/NextDigitIterator.java
@@ -8,54 +8,38 @@ import lombok.experimental.NonFinal;
 @Value
 public class NextDigitIterator implements Iterator {
 
-    @NonFinal int i = 0;
-    int[] digits;
-    int length;
+	@NonFinal int i = 0;
+	int[] digits;
+	int length;
 
-    public int[] stringToArray(String string) {
-        if (string == null || string == "") {
-            return digits;
-        }
-        decimalRemoval(string);
-        int[] digits = new int[string.length()];
-        for (int j = 0; j < string.length(); i++) {
-            if (j != digits.length - 1) {
-                String s2 = string.substring(j, j + 1);
-                int digit = Integer.parseInt(s2);
-                digits[j] = digit;
-            } else {
-                j = digits.length - 1;
-                String num = string.substring(j);
-                digits[j] = Integer.parseInt(num);
-            }
-        }
-        return digits;
-    }
+	public int[] stringToArray(String string) {
+		if (string == null || "".equalsIgnoreCase(string)) {
+			return new int[] {};
+		} else {
+			string = string.replaceAll("\\.", "");
+			int[] digits = new int[string.length()];
+			for (int j = 0; j < string.length(); j++) {
 
-    private String decimalRemoval(String string) {
-        for (int j = 0; j < string.length() - 1; j++) {
-            if (string.indexOf('.') != -1) {
-                string = string.substring(0, string.indexOf('.')) + string.substring(string.indexOf('.') + 1);
-            } else {
-                break;
-            }
-        }
-        return string;
-    }
+				digits[j] = Integer.parseInt(string.substring(j, j + 1));
+			}
+			return digits;
+		}
+	}
 
-    @Override
-    public boolean hasNext() {
-        return i < digits.length;
-    }
+	@Override
+	public boolean hasNext() {
+		boolean stop =  i < digits.length;
+		return stop;
+	}
 
-    @Override
-    public Object next() {
-        return digits[i++];
-    }
+	@Override
+	public Object next() {
+		return digits[i++];
+	}
 
-    public NextDigitIterator(String string) {
-        this.digits = stringToArray(string);
-        this.length = digits.length;
-    }
+	public NextDigitIterator(String string) {
+		this.digits = stringToArray(string);
+		this.length = digits.length;			
+	}
 
 }
--- src/test/java/testRun/NextDigitIteratorTest.java
+++ src/test/java/testRun/NextDigitIteratorTest.java
@@ -1,55 +1,74 @@
 package testRun;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NextDigitIteratorTest {
 
-    /*The dataProvider will pass a String and an int[]
+	/*The dataProvider will pass a String and an int[]
 The test will use the String to create a NextDigitIterator
 It will then loop through the digits of the array an iterator to check if they’re the same
 It will also than check that the iterator says it doesn’t have anything left
-     */
+	 */
 
-    @Test
-    public void hasNextTest() {
+	@Test
+	public void hasNextTest() {
+		NextDigitIterator ndi = new NextDigitIterator("5.4.3.2.1.");
 
-    }
+		for (int i = 0; i < 5; i ++)
+			if (ndi.hasNext()) {
+				
+			}
 
-    @Test(dataProvider="nextTestDP")
-    public void nextTest(NextDigitIterator ndi, int[] expected) {
-        int length = expected.length;
-        assertEquals(ndi.next(), expected);
-        for(int i = 0; i < length; i++) {
-            assertEquals(Integer.parseInt(ndi.next().toString()), expected[i]);
-        }
-    }
 
-    @DataProvider
-    Object[][] nextTestDP() {
-        return new Object[][] {
-            // nothing in, nothing out. handles null
-            {new NextDigitIterator(null), new int[] {}},
-            {new NextDigitIterator(""), new int[] {}},
 
-            // dealing with decimals
-            {new NextDigitIterator("...."), new int[] {}},
-            {new NextDigitIterator("."), new int[] {}},
-            {new NextDigitIterator(".5683."), new int[] {5,6,8,3}},
-            {new NextDigitIterator("5.4.3.2.1."), new int[] {5,4,3,2,1}},
 
-            // same num any amount of times
-            {new NextDigitIterator("5"), new int[] {5}},
-            {new NextDigitIterator("333333"), new int[] {3,3,3,3,3,3}},
+		int bigInt = ndi.toString().length() + 12;
+		for (int i = 0; i < bigInt; i++) {
+			if (i < ndi.toString().length()) {
+				assertTrue(ndi.hasNext());
+			} else {
+				assertFalse(ndi.hasNext());
+				break;
+			}        	
+		}
+	}
 
-            // just tests
-            {new NextDigitIterator("453027"), new int[] {4,5,3,0,2,7}},
-            {new NextDigitIterator("123456"), new int[] {1,2,3,4,5,6}},
-            {new NextDigitIterator("3.14159"), new int[] {3,1,4,1,5,9}},
-            {new NextDigitIterator("5."), new int[] {5}},
-            {new NextDigitIterator(".5"), new int[] {5}},
-        };
-    }
+	@Test(dataProvider="nextTestDP")
+	public void nextTest(NextDigitIterator ndi, int[] expected) {
+		int length = expected.length;
+		for(int i = 0; i < length; i++) {
+			assertEquals(Integer.parseInt(ndi.next().toString()), expected[i]);
+		}
+	}
+
+	@DataProvider
+	Object[][] nextTestDP() {
+		return new Object[][] {
+			// nothing in, nothing out. handles null
+			{new NextDigitIterator(null), new int[] {}},
+			{new NextDigitIterator(""), new int[] {}},
+
+			// dealing with decimals
+			{new NextDigitIterator("...."), new int[] {}},
+			{new NextDigitIterator("."), new int[] {}},
+			{new NextDigitIterator(".5683."), new int[] {5,6,8,3}},
+			{new NextDigitIterator("5.4.3.2.1."), new int[] {5,4,3,2,1}},
+
+			// same num any amount of times
+			{new NextDigitIterator("5"), new int[] {5}},
+			{new NextDigitIterator("333333"), new int[] {3,3,3,3,3,3}},
+
+			// just tests
+			{new NextDigitIterator("453027"), new int[] {4,5,3,0,2,7}},
+			{new NextDigitIterator("123456"), new int[] {1,2,3,4,5,6}},
+			{new NextDigitIterator("3.14159"), new int[] {3,1,4,1,5,9}},
+			{new NextDigitIterator("5."), new int[] {5}},
+			{new NextDigitIterator(".5"), new int[] {5}},
+		};
+	}
 }
