df8297480ee174d37b2906a95d735c28fbb1353a
Dorian Abraham <>
2020-03-23 18:06:22 -0400

    MADE PERFECT AMAZING CODE THAT WORKS FOR NUMSTOWORDSITERATOR!!!!!!!!!!

--- src/main/java/testRun/NumsToWordsIterator.java
+++ src/main/java/testRun/NumsToWordsIterator.java
@@ -2,47 +2,52 @@ package testRun;
 
 import java.util.Iterator;
 
-public class NumsToWordsIterator implements Iterator {
-	
-	static String string;
-	int i = 0;
-	static char[] charArray;
-	
-	public static String numsToWords() {
-		string = string.replaceAll("1", "one");
-		string = string.replaceAll("2", "two");
-		string = string.replaceAll("3", "three");
-		string = string.replaceAll("4", "four");
-		string = string.replaceAll("5", "five");
-		string = string.replaceAll("6", "six");
-		string = string.replaceAll("7", "seven");
-		string = string.replaceAll("8", "eight");
-		string = string.replaceAll("9", "nine");
-		string = string.replaceAll("0", "zero");
-		return string;
-	}
-	
-	public static char[] stringToArray() {
-		char[] charArray = new char[string.length() - 1];
-		
-		for (int j = 0; j < string.length() - 1; j++) {
+public class NumsToWordsIterator implements Iterator<Character> {
 
-			charArray[j] = string.charAt(j);
-		}
-		return charArray;
-	}
-	
-	@Override
-	public boolean hasNext() {
-		return i < string.length() - 1;
-	}
+    int i = 0;
+    final int length;
+    final char[] charArray;
 
-	@Override
-	public Object next() {
-		i++;
-		return charArray[i];
-	}
+    public static String numsToWords(String string) {
+        //replaces non-numeric chars with ""
+        string = string.replaceAll("[^\\d.]", "");
+        //turns out "." is numeric
+        string = string.replaceAll("\\.", "");
 
-	
-	
+        string = string.replaceAll("1", "one");
+        string = string.replaceAll("2", "two");
+        string = string.replaceAll("3", "three");
+        string = string.replaceAll("4", "four");
+        string = string.replaceAll("5", "five");
+        string = string.replaceAll("6", "six");
+        string = string.replaceAll("7", "seven");
+        string = string.replaceAll("8", "eight");
+        string = string.replaceAll("9", "nine");
+        string = string.replaceAll("0", "zero");
+
+        return string;
+    }
+
+    public static char[] stringToArray(String string) {
+        if (string == null || "".equalsIgnoreCase(string)) {
+            return new char[] {};
+        } else {
+            return numsToWords(string).toCharArray();
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return i < charArray.length;
+    }
+
+    @Override
+    public Character next() {
+        return charArray[i++];
+    }
+
+    public NumsToWordsIterator(String string) {
+        this.charArray = stringToArray(string);
+        this.length = charArray.length;
+    }
 }
--- src/test/java/testRun/NumsToWordsIteratorTest.java
+++ src/test/java/testRun/NumsToWordsIteratorTest.java
@@ -1,16 +1,42 @@
 package testRun;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NumsToWordsIteratorTest {
 
-  @Test
-  public void hasNextTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @Test(dataProvider = "nextTestDP")
+    public void hasNextTest(String input, char[] expected) {
+        NumsToWordsIterator ntwi = new NumsToWordsIterator(input);
 
-  @Test
-  public void nextTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+        for (int i = 0; i < expected.length; i ++) {
+            assertTrue(ntwi.hasNext());
+            assertEquals(ntwi.next(), (Character)expected[i]);
+        }
+        assertFalse(ntwi.hasNext());
+    }
+
+    @DataProvider
+    Object[][] nextTestDP() {
+        return new Object[][] {
+            // nothing in, nothing out. handles null
+            {null, new char[] {}},
+            {"", new char[] {}},
+            {".", new char[] {}},
+
+            // same num any amount of times
+            {"5", new char[] {'f','i','v','e'}},
+            {"333333", new char[] {'t','h','r','e','e','t','h','r','e','e','t','h','r','e','e','t','h','r','e','e','t','h','r','e','e','t','h','r','e','e',}},
+
+            // just tests
+            {"5-", new char[] {'f','i','v','e'}},
+            {"4530", new char[] {'f','o','u','r','f','i','v','e','t','h','r','e','e','z','e','r','o'}},
+            {".5683.", new char[] {'f','i','v','e','s','i','x','e','i','g','h','t','t','h','r','e','e'}},
+            {"5.4.3.2.1.", new char[] {'f','i','v','e','f','o','u','r','t','h','r','e','e','t','w','o','o','n','e'}},
+        };
+    }
 }
