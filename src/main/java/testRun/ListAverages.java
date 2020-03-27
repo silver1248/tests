794df3f8c033de65ef4833be811a9838da10080e
Dorian Abraham <>
2020-03-27 16:00:47 -0400

    i think im done with the code im going to start making tests later

--- src/main/java/testRun/ListAverages.java
+++ src/main/java/testRun/ListAverages.java
@@ -13,42 +13,44 @@ public class ListAverages {
     //        list = list.reverse();
     //        System.out.println(list);
     //    }
-//    public static List<Float> averageList(List numList) {
-//        float average;
-//        int length = numList.toArray().length();
-//        List<Float> floatList = List.empty();
-//        int listEntry = 0;
-//
-//        for (int i = 1; i < length; i++) {
-//            for (int j = length -1; j < i; j--) {
-////                listEntry += makeNewNum();
-//            }
-//            listEntry = 0;
-//        }
-//        return null;
-//    }
-
-    public static void main(String[] args) {
-        System.out.println(smallerString(5));
-        System.out.println(tofloatList(smallerString(5)));
+    public static List<Float> averageList(List numList) {
+        List<Float> floatList = List.empty();
+        int length = numList.length();
+        float toBeAveraged = 0;
+        
+        for (int i = 0; i < length; i++) {
+            if (i > 0) {
+                for (int j = 0; j < i; j++) {
+                    toBeAveraged += (float) numList.get(j);
+                }
+            }
+            floatList.prepend(toBeAveraged / i - 1);
+            toBeAveraged = 0;
+        }
+        return floatList.reverse();
     }
+
+    //    public static void main(String[] args) {
+    //        System.out.println(smallerString(5));
+    //        System.out.println(tofloatList(smallerString(5)));
+    //    }
     public static List<Integer> smallerString (int stop) {
-//        List<Integer> list = List.empty();
-//        for (int i = 0; i < stop; i++) {
-//            list = list.prepend(i);
-//        }
-//        return list.reverse();
+        //        List<Integer> list = List.empty();
+        //        for (int i = 0; i < stop; i++) {
+        //            list = list.prepend(i);
+        //        }
+        //        return list.reverse();
         return List.range(0, stop);
     }
-    
-    public static List<Float> tofloatList(List<Integer> intList) {
-//        List<Float> list = List.empty();
-//
-//        for (int number : intList) {
-//            float flo = (float) number;
-//            list = list.prepend(flo);
-//        }
-//        return list.reverse();
+
+    public static List<Float> toFloatList(List<Integer> intList) {
+        //        List<Float> list = List.empty();
+        //
+        //        for (int number : intList) {
+        //            float flo = (float) number;
+        //            list = list.prepend(flo);
+        //        }
+        //        return list.reverse();
         return intList.map(i -> (float) i);
     }
 }
---
+++ src/main/java/testRun/ListAveragesTest.java
@@ -0,0 +1,11 @@
+package testRun;
+
+import org.testng.annotations.Test;
+
+public class ListAveragesTest {
+
+  @Test
+  public void averageListTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
