fb76433121e22ed8f002ac8075a985f9123db2df
Dorian Abraham <>
2020-03-29 17:30:58 -0400

    I'm trying to make the tests work but I'm having some trouble with errors

--- src/main/java/testRun/ListAveragesTest.java
+++ src/main/java/testRun/ListAveragesTest.java
@@ -1,11 +1,27 @@
 package testRun;
 
+import static org.testng.Assert.assertEquals;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ListAveragesTest {
 
-  @Test
-  public void averageListTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @Test(dataProvider="averageListTestDP")
+    public void averageListTest(List<Integer> in, List<Float>) {
+        expected = ListAverages.averageList(in);
+    }
+
+    @DataProvider
+    Object[][] averageListTestDP() {
+        return new List[][] {
+            {new List(1,2,3), List<Float>(1.5, 2.0)},
+
+            {},
+            {},
+
+            {},
+        };
+    }
 }
