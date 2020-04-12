67fd554a4246e2a0712624b8414deed987297951
Dorian Abraham <>
2020-04-12 18:45:34 -0400

    made a thing that checks if a thing cubed is greater than 1000 and prints out the cubed things that are greater than 1000 in the order they are in the list

--- src/main/java/testRun/CubesBiggerThan1000.java
+++ src/main/java/testRun/CubesBiggerThan1000.java
@@ -4,16 +4,7 @@ import io.vavr.collection.List;
 
 public class CubesBiggerThan1000 {
 
-    public static String numsOverCubedThousand(List<Integer> in) {
-        String nums = "";
-        List<Integer> intList = in.map(x -> x*x*x).filter(x -> (x > 1000));
-        if (intList.isEmpty()) {
-            return "";
-        } else {
-            for (int i = 0; i < intList.length(); i++) {
-                nums = nums + intList.get(i);
-            }
-            return nums;
-        }
+    public static String numsCubedOverThousand(List<Integer> in) {
+        return in.map(x -> x*x*x).filter(x -> (x > 1000)).map(x -> String.valueOf(x)).fold("", (x,y) -> x+y);
     }
 }
